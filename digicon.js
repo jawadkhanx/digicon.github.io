1560b9cbea07e8fb8f76d3242abad9cfb06a392e
jawadkhanx <jdkhan7x@gmail.com>
2022-10-20 12:10:18 -0700

    Initial commit

---
+++ digicon.js
@@ -0,0 +1,269 @@
+document.addEventListener('keyup', (event) => {
+    var name = event.key;
+  
+    
+    // Alert the alkey name and key code on keydown
+    if(name==1){
+        var greeting = document.getElementById("greeting");
+          greeting.play();
+    }
+          if(name==2){
+              var pitch = document.getElementById("pitch");
+                pitch.play();
+          }
+          if(name==3){
+            var dob = document.getElementById("dob");
+              dob.play();
+        }
+       
+
+          else if(name== 'Control'){
+              var greeting = document.getElementById("greeting");
+              var pitch = document.getElementById("pitch");
+              var dob =document.getElementById('dob');
+              var medicareab =document.getElementById('medicareab');
+              var transfer = document.getElementById("transfer");
+              var bye = document.getElementById("bye");
+              var notinterested =document.getElementById('notinterested');
+              var busy =document.getElementById('busy');
+              var real = document.getElementById("real");
+              var there = document.getElementById("there");
+              var yes =document.getElementById('yes');
+              var no =document.getElementById('no');
+              var haveplan =document.getElementById('haveplan');
+              var ok =document.getElementById('ok');
+              
+    var hello = document.getElementById("hello");
+    var zip =document.getElementById('zip');
+    var thanks =document.getElementById('thanks');
+    var glad = document.getElementById("glad");
+    var sorry = document.getElementById("sorry");
+    var repeat =document.getElementById('repeat');
+    var name =document.getElementById('name');
+    var location =document.getElementById('location');
+    var dob1 =document.getElementById('dob1');
+    hello.pause();
+    hello.currentTime = 0;
+    zip.pause();
+    zip.currentTime = 0;
+    dob1.pause();
+    dob1.currentTime = 0;
+    thanks.pause();
+    thanks.currentTime = 0;
+    glad.pause();
+    glad.currentTime = 0;
+    sorry.pause();
+    sorry.currentTime = 0;
+    repeat.pause();
+    repeat.currentTime = 0;
+    name.pause();
+    name.currentTime = 0;
+    location.pause();
+    location.currentTime = 0;
+             
+              greeting.pause();
+              greeting.currentTime = 0;
+              pitch.pause();
+              pitch.currentTime = 0;
+              dob.pause();
+              dob.currentTime = 0;
+              medicareab.pause();
+              medicareab.currentTime = 0;
+              transfer.pause();
+              transfer.currentTime = 0;
+              bye.pause();
+              bye.currentTime = 0;
+              notinterested.pause();
+              notinterested.currentTime = 0;
+              yes.pause();
+              yes.currentTime = 0;
+              no.pause();
+              no.currentTime = 0;
+              there.pause();
+              there.currentTime = 0;
+              ok.pause();
+              ok.currentTime = 0;
+              real.pause();
+              real.currentTime = 0;
+              haveplan.pause();
+              haveplan.currentTime = 0;
+              busy.pause();
+              busy.currentTime = 0;
+              
+            
+            
+            
+           
+            
+           
+          }
+ }, false);
+function stop(){
+    var greeting = document.getElementById("greeting");
+    var pitch = document.getElementById("pitch");
+    var dob =document.getElementById('dob');
+    var medicareab =document.getElementById('medicareab');
+    var transfer = document.getElementById("transfer");
+    var bye = document.getElementById("bye");
+    var notinterested =document.getElementById('notinterested');
+    var busy =document.getElementById('busy');
+    var real = document.getElementById("real");
+    var there = document.getElementById("there");
+    var yes =document.getElementById('yes');
+    var no =document.getElementById('no');
+    var haveplan =document.getElementById('haveplan');
+    var ok =document.getElementById('ok');
+
+    var hello = document.getElementById("hello");
+    var zip =document.getElementById('zip');
+    var thanks =document.getElementById('thanks');
+    var glad = document.getElementById("glad");
+    var sorry = document.getElementById("sorry");
+    var repeat =document.getElementById('repeat');
+    var name =document.getElementById('name');
+    var location =document.getElementById('location');
+    var dob1 =document.getElementById('dob1');
+   
+    greeting.pause();
+    greeting.currentTime = 0;
+    pitch.pause();
+    pitch.currentTime = 0;
+    dob.pause();
+    dob.currentTime = 0;
+    medicareab.pause();
+    medicareab.currentTime = 0;
+    transfer.pause();
+    transfer.currentTime = 0;
+    bye.pause();
+    bye.currentTime = 0;
+    notinterested.pause();
+    notinterested.currentTime = 0;
+    yes.pause();
+    yes.currentTime = 0;
+    no.pause();
+    no.currentTime = 0;
+    there.pause();
+    there.currentTime = 0;
+    ok.pause();
+    ok.currentTime = 0;
+    real.pause();
+    real.currentTime = 0;
+    haveplan.pause();
+    haveplan.currentTime = 0;
+    busy.pause();
+    busy.currentTime = 0;
+
+    hello.pause();
+    hello.currentTime = 0;
+    zip.pause();
+    zip.currentTime = 0;
+    dob1.pause();
+    dob1.currentTime = 0;
+    thanks.pause();
+    thanks.currentTime = 0;
+    glad.pause();
+    glad.currentTime = 0;
+    sorry.pause();
+    sorry.currentTime = 0;
+    repeat.pause();
+    repeat.currentTime = 0;
+    name.pause();
+    name.currentTime = 0;
+    location.pause();
+    location.currentTime = 0;
+    
+  
+}
+  function greeting(){
+    var greeting =document.getElementById('greeting');
+    greeting.play();
+   }
+   function pitch(){
+    var pitch =document.getElementById('pitch');
+    pitch.play();
+   }
+   function dob(){
+    var dob =document.getElementById('dob');
+    dob.play();
+   }
+   function medicareab(){
+    var medicareab=document.getElementById('medicareab');
+    medicareab.play();
+   }
+   function transfer(){
+    var transfer =document.getElementById('transfer');
+    transfer.play();
+   }
+   function bye(){
+    var bye =document.getElementById('bye');
+    bye.play();
+   }
+   function notinterested(){
+    var notinterested =document.getElementById('notinterested');
+    notinterested.play();
+   }
+   function busy(){
+    var busy =document.getElementById('busy');
+    busy.play();
+   }
+   function haveplan(){
+    var haveplan =document.getElementById('haveplan');
+    haveplan.play();
+   }
+   function real(){
+    var real =document.getElementById('real');
+    real.play();
+   }
+   function there(){
+    var there =document.getElementById('there');
+    there.play();
+   }
+   function yes(){
+    var yes =document.getElementById('yes');
+    yes.play();
+   }
+   function no(){
+    var no =document.getElementById('no');
+    no.play();
+   }
+   function ok(){
+    var ok=document.getElementById('ok');
+    ok.play();
+   }
+
+   function hello(){
+    var hello =document.getElementById('hello');
+    hello.play();
+   }
+   function dob1(){
+    var dob1 =document.getElementById('dob1');
+    dob1.play();
+   }
+   function zip(){
+    var zip =document.getElementById('zip');
+    zip.play();
+   }
+   function thanks(){
+    var thanks=document.getElementById('thanks');
+    thanks.play();
+   }
+   function glad(){
+    var glad =document.getElementById('glad');
+    glad.play();
+   }
+   function sorry(){
+    var sorry =document.getElementById('sorry');
+    sorry.play();
+   }
+   function repeat(){
+    var repeat =document.getElementById('repeat');
+    repeat.play();
+   }
+   function namea(){
+    var name =document.getElementById('name');
+    name.play();
+   }
+   function locationa(){
+    var location=document.getElementById('location');
+    location.play();
+   }
